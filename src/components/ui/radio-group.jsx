267461f082915c2b24310bd8d936860886bdb685
Sahil <leviack629@gmail.com>
2024-03-02 00:40:26 +0530

    remove checkIcon from wrong answer

--- src/components/ui/radio-group.jsx
+++ src/components/ui/radio-group.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { CheckIcon } from "@radix-ui/react-icons"
+// import { CheckIcon } from "@radix-ui/react-icons"
 import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
 
 import { cn } from "@/lib/utils"
@@ -21,7 +21,7 @@ const RadioGroupItem = React.forwardRef(({ className, ...props }, ref) => {
       )}
       {...props}>
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <CheckIcon className="h-3.5 w-3.5 fill-primary" />
+        {/* <CheckIcon className="h-3.5 w-3.5 fill-primary" /> */}
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>)
   );
